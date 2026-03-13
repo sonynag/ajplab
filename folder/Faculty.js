ac2bfc46dbf5ebc6bdcb65aed04d4d0a9b34d054
srinaya-sudireddy <srinayasudireddy@gmail.com>
2026-03-13 13:21:56 +0530

    deleted unneccesary!!

--- folder/Faculty.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const FacultySchema = new mongoose.Schema({
-    name: String,
-    designation: String,
-    salary: Number
-});
-
-module.exports = mongoose.model("faculty", FacultySchema);
