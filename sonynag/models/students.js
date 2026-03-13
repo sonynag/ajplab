840152e9d4efa636db2a9698b94c53820982d141
srinaya-sudireddy <srinayasudireddy@gmail.com>
2026-03-13 13:18:16 +0530

    qb commit

---
+++ sonynag/models/students.js
@@ -0,0 +1,7 @@
+const mongoose = require("mongoose");
+const StudentSchema = new mongoose.Schema({
+    name : String,
+    age : Number,
+    course : String
+});
+module.exports = mongoose.model("students", StudentSchema);
