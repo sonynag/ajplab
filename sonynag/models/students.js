8bd1159a011c721a0bf43ed2a4bd779d17fb5129
sonynag <gsonynag@gmail.com>
2026-04-10 15:48:13 -0400

    jwt auth

--- sonynag/models/students.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require("mongoose");
-const StudentSchema = new mongoose.Schema({
-    name : String,
-    age : Number,
-    course : String
-});
-module.exports = mongoose.model("students", StudentSchema);
