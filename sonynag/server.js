a67b251fa61260ec5d35e382edc7ddc0d9348b57
sonynag <gsonynag@gmail.com>
2026-04-10 15:23:13 -0400

    my edit

--- sonynag/server.js
+++ sonynag/server.js
@@ -4,7 +4,7 @@ const bodyParser = require("body-parser");
 
 const studentsRoutes = require("./routes/studentsRoutes");
 const facultyRoutes = require("./routes/facultyRoutes");
-//
+////
 const app = express();
 app.use(bodyParser.json());
 
